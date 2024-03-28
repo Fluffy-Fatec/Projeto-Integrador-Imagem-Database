d52aa4e13976497d27335a0c8070688b2fdc6e2d
Aldrik Alvaro <51426536+Aldrik-Alvaro@users.noreply.github.com>
2024-03-28 14:57:23 -0300

    FA-29 refinamento ddl.sql

--- ddl.sql
+++ ddl.sql
@@ -1,4 +1,7 @@
+DROP TABLE IF EXISTS review;
+DROP TABLE IF EXISTS invite;
 DROP TABLE IF EXISTS app_user;
+
 CREATE TABLE app_user (
     id SERIAL PRIMARY KEY,
     username VARCHAR(255) UNIQUE,
@@ -11,17 +14,15 @@ CREATE TABLE app_user (
     creationDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-DROP TABLE IF EXISTS invite;
 CREATE TABLE invite (
     id SERIAL PRIMARY KEY,
     email VARCHAR(255),
-    solicitante VARCHAR(255),
+    solicitante SERIAL,
     tokenInvite VARCHAR(255),
     creationDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT fk_solicitante FOREIGN KEY (solicitante) REFERENCES app_user(id)
 );
 
-DROP TABLE IF EXISTS review;
 CREATE TABLE review (
     id SERIAL PRIMARY KEY,
     review TEXT,
