a1ce91749ac3ecc0826c406cf6dfbdf9bf23e27e
Michael Felipe Santos Silva <118699747+heyMichaelS@users.noreply.github.com>
2024-06-05 00:50:31 -0300

    Revert "Feat: FA-144 sistema de contigencia"

--- apidelete/main.py
+++
@@ -1,8 +0,0 @@
-
-from delete import deleteMain
-
-if __name__ == "__main__":
-    ### delete ##
-    obj_project = deleteMain.projectDeleteMain()
-    obj_project.run()
-
--- apiemail/main.py
+++
@@ -1,5 +0,0 @@
-from contigencia import emailMain
-
-if __name__ == "__main__":
-    obj_project = emailMain.projectEmailMain()
-    obj_project.run()
